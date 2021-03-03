a02d5325b05dd63781c693578ec9e79b251e66cc
LeePhoter <estarg51@gmail.com>
2021-03-03 22:49:34 +0900

    added Login button

--- tracking-progress/src/component/App.js
+++ tracking-progress/src/component/App.js
@@ -1,11 +1,11 @@
 function App() {
-  return (
-    <div>
-      <header>
-        <p>tracking-progress</p>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <p>tracking-progress</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
