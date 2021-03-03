a14df3b7fdfdb86f67a2bc0d8d609378e3af8cea
Dev-miniyoung <miniyoung37@gmail.com>
2021-03-03 23:32:32 +0900

    파일 위치 수정

--- tracking-progress/src/component/App.js
+++ tracking-progress/src/component/App.js
@@ -1,11 +1,11 @@
 // import { render, screen } from "@testing-library/react";
-import React, { Component } from "react";
-import Login from "./component/Login";
+import React, { Component } from 'react';
+import Login from './Login';
 
 class App extends Component {
-    render() {
-        return <Login />;
-    }
+  render() {
+    return <Login />;
+  }
 }
 // test("renders learn react link", () => {
 //     render(<App />);
