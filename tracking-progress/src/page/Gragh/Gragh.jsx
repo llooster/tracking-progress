1377cdee8b59a66a169dd0592f5b2422dae6550d
LeePhoter <estarg51@gmail.com>
2021-03-11 00:26:53 +0900

    json파일 값에 따른 id대입

--- tracking-progress/src/page/Gragh/Gragh.jsx
+++ tracking-progress/src/page/Gragh/Gragh.jsx
@@ -7,35 +7,42 @@ import Table from "../../component/table/Table.json";
 
 class Gragh extends PureComponent {
     state = {
-        level: [],
+        name: Object.keys(Table),
+        // level: Object.values(Table),
+        value: Object.values(Table).map((each, index) => {
+            if (each === "1") {
+                return "absence";
+            } else if (each === "2") {
+                return "late";
+            } else {
+                return "attendance";
+            }
+        }),
     };
-    // getLevel = () => {
-    //     this.setState({
-    //         level: Table.map((item, index) => {
-    //             // return item;
-    //         }),
-    //     });
-    // };
     render() {
-        // let newTable = Table.map((item, index) => {
-        //     item.a
-        // });
-        // this.getLevel();
-        let { level } = this.state;
-
-        console.log(level);
+        let { name, level, value } = this.state;
+        console.log("name :>> ", name);
+        // console.log("level :>> ", level);
         return (
             <div className="graghWhole">
                 <div className="gragh">
-                    <div id="absence">1</div>
-                    <div id="late">2</div>
-                    <div id="attendance">3</div>
-                    <div>4</div>
-                    <div>5</div>
-                    <div>6</div>
-                    <div>7</div>
+                    <div id={value[0]}>1</div>
+                    <div id={value[1]}>2</div>
+                    <div id={value[2]}>3</div>
+                    <div id={value[3]}>4</div>
+                    <div id={value[4]}>5</div>
+                    <div id={value[5]}>6</div>
+                    <div id={value[6]}>7</div>
                 </div>
-                <div className="color"></div>
+                <div className="color">
+                    <span>{name[0]}</span>
+                    <span>{name[1]}</span>
+                    <span>{name[2]}</span>
+                    <span>{name[3]}</span>
+                    <span>{name[4]}</span>
+                    <span>{name[5]}</span>
+                    <span>{name[6]}</span>
+                </div>
             </div>
         );
     }
