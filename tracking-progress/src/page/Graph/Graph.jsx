7afa7bc33e5ac1617b21e19f84976720e6a37336
LeePhoter <estarg51@gmail.com>
2021-03-23 23:57:50 +0900

    들어야하는 다음 수업 교시

--- tracking-progress/src/page/Graph/Graph.jsx
+++ tracking-progress/src/page/Graph/Graph.jsx
@@ -12,14 +12,11 @@ class Graph extends PureComponent {
         name: [], //수업이름
         firstName: [], //수업이름앞글자
         attendanceWeek: [], //총 16개 (주차 출석)
-        attendance: [],
-        //[["O", "X", "X"],["X", "X", "X"],["", "", ""], ...]
-        // name이 1로시작하는것들
+        attendance: [], //[["O", "X", "X"],["X", "X", "X"],["", "", ""], ...]
+        lastAttendance: [], //출석에 맞는 id값
+        class1: [],
         graphCount: [0, 1, 2, 3, 4, 5, 6], //과목수 7
         status: [1, 2], //16주차를 해야됨
-        attendWeek: dummy2[0].progress.attendanceWeek,
-        attend: dummy2[0].progress.attendance,
-        lastAttendance: [], //출석에 맞는 id값
     };
 
     componentDidMount() {
@@ -29,23 +26,23 @@ class Graph extends PureComponent {
     }
 
     getName = () => {
-        //수업명 가져오기
+        // 수업명 가져오기
         const { name } = this.state;
-        const getName = Array.from(name);
-        getName.push(dummy2[0].course.name);
-        //나중엔 이렇게 해야됨
+        const _name = Array.from(name);
+        _name.push(dummy2[0].course.name);
+        // 나중엔 이렇게 해야됨
         // graphCount.map((item) => {
         //     dummy2[item].course.name;
         // });
         this.setState({
-            name: getName,
+            name: _name,
         });
         this.getFirstName(dummy2[0].course.name);
     };
 
     getFirstName = (each) => {
         const firstname = each.split("")[0];
-        //나중엔 이렇게 해야됨
+        // 나중엔 이렇게 해야됨
         // graphCount.map((item) => {
         //     name[item].split("")[0];
         // });
@@ -58,7 +55,7 @@ class Graph extends PureComponent {
         //
         const { status } = this.state;
         const _attendance = status.map((each, index) => {
-            return [dummy2[0].progress[index].map((item) => item.attendance)];
+            return dummy2[0].progress[index].map((item) => item.attendance);
         });
         this.setState({
             attendance: _attendance,
@@ -71,6 +68,7 @@ class Graph extends PureComponent {
             attendanceWeek: _attendanceWeek,
         });
         this.judgeAttendance(_attendanceWeek);
+        this.takeClass(_attendance, _attendanceWeek);
     };
 
     judgeAttendance = (_attendanceWeek) => {
@@ -79,27 +77,64 @@ class Graph extends PureComponent {
         const _lastAttendance = _attendanceWeek.map((each, index) => {
             if (each === "O") {
                 return "attendance";
-            } else if (each === "▲") {
-                return "late";
             } else {
                 return "absence";
             }
         });
+        //      else if (each === "▲") {
+        //         return "late";
+        //     } else {
+        //         return "absence";
+        //     }
+        // });
 
         this.setState({
             lastAttendance: _lastAttendance,
         });
     };
 
+    takeClass = (_attendance, _attendanceWeek) => {
+        //몇교시부터 들어야 하는가
+        const _class = _attendance[_attendanceWeek.indexOf("X")].map(
+            (item, index) => {
+                if (item !== "O") {
+                    return index + 1;
+                } else {
+                    return null;
+                }
+            }
+        );
+        let __class = _class.sort()[0];
+        this.setState({
+            class1: __class,
+        });
+
+        // attendance[attendanceWeek.indexOf("X")]
+    };
+
     render() {
         const {
             name,
             firstName,
+            class1,
+            attendance,
             attendanceWeek,
             lastAttendance,
             graphCount,
         } = this.state;
-        console.log(lastAttendance);
+
+        // console.log("attendance :>> ", attendance);
+        // console.log("attendanceWeek :>> ", attendanceWeek);
+        // console.log(attendance[attendanceWeek.indexOf("X")]);
+        // console.log(
+        //     'attendance[attendanceWeek.indexOf("X")] :>> ',
+        //     attendance[attendanceWeek.indexOf("X")]
+        // );
+        // console.log(
+        //     "몇교시수업부터 들어야 되는가 :>> ",
+        //     attendance[attendanceWeek.indexOf("X")]
+        // );
+        console.log("몇교시부터 들어야되냐? :>> ", class1);
         return (
             <div className="graghWhole">
                 <div className="gragh">
